21f273e6511aa17156e1ae64a84498a3267c18c5
loganscodes <logansafar11@gmail.com>
2024-08-15 23:23:54 -0600

    style: :art: acordion

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,8 +1,30 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+
+type Content = {
+    id: number;
+    name: string;
+    subtitle: string;
+}
+
+const content: Content[] = [
+    { id: 1, name: "Is it accessible?", subtitle: "Yes. It adheres to the WAI-ARIA design pattern." },
+    { id: 2, name: "Is it styled?", subtitle: "Yes. It comes with default styles that matches the other components&apos; aesthetic." },
+    { id: 3, name: "Soy bien vergas", subtitle: "Soy el Rey" }
+];
 
 export default function NamePage() {
     return (
-        <div>
-            <h1>Accordion Page</h1>
-        </div>
+            <Accordion type="single" collapsible>
+                {
+                    content.map(({ name, subtitle }) => (
+                        <AccordionItem key={name} value={name}>
+                            <AccordionTrigger>{name}</AccordionTrigger>
+                            <AccordionContent>
+                                {subtitle}
+                            </AccordionContent>
+                        </AccordionItem>
+                    ))
+                }
+            </Accordion>
     );
 }
